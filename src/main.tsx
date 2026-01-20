00fc14a863ecfb1c910fea78ace1a760ba60a5db
Evgeniya Kosykh <evgeniyapylypenko92@gmail.com>
2026-01-20 14:55:43 +0200

    fix bug and add all file

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
-import { StrictMode } from "react";
+import React from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
-import App from "./components/App/App.tsx";
+import App from "./components/App/App";
+import "modern-normalize/modern-normalize.css";
+import { Toaster } from "react-hot-toast";
 
 createRoot(document.getElementById("root")!).render(
-  <StrictMode>
+  <React.StrictMode>
     <App />
-  </StrictMode>,
+    <Toaster />
+  </React.StrictMode>,
 );
