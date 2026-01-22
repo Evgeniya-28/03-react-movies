47e2e7940551a0e5b174d6af087a98c9e9f38b21
Evgeniya Kosykh <evgeniyapylypenko92@gmail.com>
2026-01-22 19:58:36 +0200

    fix

--- src/services/movieService.ts
+++ src/services/movieService.ts
@@ -1,33 +1,22 @@
 import axios from "axios";
 import type { Movie } from "../types/movie";
 
-const api = axios.create({
-  baseURL: "https://api.themoviedb.org/3",
-});
-
-api.interceptors.request.use((config) => {
-  const token = import.meta.env.VITE_TMDB_TOKEN;
-
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
-  }
-
-  return config;
-});
+const BASE_URL = "https://api.themoviedb.org/3";
+const TOKEN = import.meta.env.VITE_TMDB_TOKEN;
 
 interface FetchMoviesResponse {
   results: Movie[];
 }
 
 export async function fetchMovies(query: string): Promise<Movie[]> {
-  const response = await api.get<FetchMoviesResponse>("/search/movie", {
-    params: {
-      query,
-      page: 1,
-      include_adult: false,
-      language: "en-US",
+  const response = await axios.get<FetchMoviesResponse>(
+    `${BASE_URL}/search/movie`,
+    {
+      params: { query },
+      headers: {
+        Authorization: `Bearer ${TOKEN}`,
+      },
     },
-  });
-
+  );
   return response.data.results;
 }
